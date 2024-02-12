c27d022c96e7e1e744e89f1a75f88e6e3968629a
lokalakorvgubben <william.andersson5@elev.ga.ntig.se>
2024-02-12 13:10:21 +0100

    asdffds

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
     <title>This index</title>
 </head>
 <body>
+    <?php
+    if (isset($_SESSION["user"])) {
+        ?>
+        <a href="logout.php" type="button" class="btn btn-outline-info" style="height: 50px; margin: 10px;color:aliceblue;position: absolute;padding: 11;right: 0px;margin-right: 200px;font-weight: 600;width: 75px;">Logout</a>  
+        <?php
+    }
+    else{
+        ?>
+        <a href="login.php" type="button" class="btn btn-outline-info" style="height: 50px; margin: 10px;color:aliceblue;position: absolute;padding: 11;right: 0px;margin-right: 200px;font-weight: 600;width: 75px;">Login</a>  
+        <?php
+    }
+    ?>
     <h1>kaboom</h1>
     <p>kaboom 2</p>
     <p>kaboom 3</p>
